6bf33fe94c8326e8de04ad8a5ec6099f08585848
kefimaher <kefi.maher@esprit.tn>
2023-06-06 09:17:29 +0100

    06/06/2023  09:17

--- src/Controller/CongeController.php
+++ src/Controller/CongeController.php
@@ -14,12 +14,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class CongeController extends  AbstractController
 {
     #[Route('/congelist', name: 'congelist')]
+
     public function congeslistAction(ManagerRegistry $doctrine ) : Response
     {
+        // recupure les list des conge statut en attente
         $conge = $doctrine->getRepository(Conge::class);
         $listconge=$conge->findAll();
         return $this->render('conge/congelist.html.twig',array('conges' => $listconge));
     }
+
+    #[Route('/historique', name: 'historique')]
+    public function historiqueAction(ManagerRegistry $doctrine ) : Response
+    {
+        // recupure les list des conge statut accpeter ou refuse
+        $conge = $doctrine->getRepository(Conge::class);
+        $listconge=$conge->findAll();
+        return $this->render('conge/historique.html.twig',array('conges' => $listconge));
+    }
+
 
     #[Route('/demandeconge', name: 'demandeconge')]
     public function demandecongeAction(ManagerRegistry $doctrine ,Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
@@ -29,6 +41,9 @@ class CongeController extends  AbstractController
         $form = $this->createForm(CongeFormType::class, $conge);
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
+            $startday=$form->get('start_day')->getData() ;
+            $endday= $form->get('end_day')->getData() ;
+            /* calculer la deffrence enter les deux date */
             $conge->setStartDay($form->get('start_day')->getData());
             $conge->setEndDay($form->get('end_day')->getData());
             $conge->setTypeConge($form->get('type_conge')->getData());
@@ -50,40 +65,45 @@ class CongeController extends  AbstractController
         $repository->flush();
         return $this->redirectToRoute('congelist');
     }
-
-
     #[Route('/accepter/{id}/{nbj}', name: 'accepter')]
-    public function accepterAction(ManagerRegistry $doctrine ,$id)
+    public function accepterAction(ManagerRegistry $doctrine ,$id , $nbj)
     {
+        // recupure  l'employer qui faire la demande
         $repository = $doctrine->getRepository(Conge::class);
         $conge=$repository->findOneBy(array('id' => $id));
-
+        echo ($nbj) ;
+        // die();
         if ($conge) {
             $type = $conge->getType();
             if ($type == 'maladie') {
                 //  la soustraction depuis les jours maladie
-            } else {
+            }
+            else
+            {
                 //  la soustraction depuis les jours annuelle
             }
-
             $conge->setStatuts("accepter");
             $repository->persist($conge);
             $repository->flush();
             }
         return $this->redirectToRoute('congelist');
     }
-    /*
-    #[Route('/supprime/{id}', name: 'supprime')]
-    public function supprimeAction(ManagerRegistry $doctrine ,$id)
+
+        #[Route('/refuse/{id}', name: 'refuse')]
+    public function refuseAction(ManagerRegistry $doctrine ,$id)
     {
         $repository = $doctrine->getRepository(Conge::class);
         $conge=$repository->findOneBy(array('id' => $id));
-        $repository->remove($conge);
-        // $repository->flush();
+        if ($conge)
+        {
+            $conge->setStatuts("refuser");
+            $repository->persist($conge);
+            $repository->flush();
+        }
         return $this->redirectToRoute('congelist');
     }
 
-    */
+
 
 }
 
