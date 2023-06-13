17c1be59abc271633e1197629f5087e6955bc909
kefimaher <kefi.maher@esprit.tn>
2023-06-13 09:32:05 +0100

    09:31  13/06/2023

--- src/Controller/receptionController.php
+++ src/Controller/receptionController.php
@@ -3,8 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Conge;
-use App\Entity\UserProfile;
-use App\Form\CongeFormType;
+use App\Entity\Reception;
+use App\Form\ReceptionFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,46 +15,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class receptionController extends AbstractController
 {
-    #[Route('/receptionlist', name: 'receptionlist')]
+    #[Route('/demanderhlist', name: 'demanderhlist')]
 
     public function receptionlistAction(ManagerRegistry $doctrine ) : Response
     {
         $reception = $doctrine->getRepository(Conge::class);
         $listreception=$reception->findAll();
-        return $this->render('reception/receptionlist.html.twig',array('reception' => $listreception));
+        return $this->render('demanderh/demanderhlist.html.twig',array('reception' => $listreception));
     }
-
-/*
-    #[Route('/demandeconge', name: 'demandeconge')]
+    #[Route('/demanderh', name: 'demanderh')]
     public function demandecongeAction(ManagerRegistry $doctrine ,Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $iduser = $this->getUser()->getId();
         $userprofile = $doctrine->getRepository(UserProfile::class)->findOneBy(array('id' => $iduser));
-        $repository = $doctrine->getRepository(Conge::class);
-        $conge = new Conge() ;
-        $form = $this->createForm(CongeFormType::class, $conge);
+        $demanderh = new Reception() ;
+        $form = $this->createForm(ReceptionFormType::class, $demanderh);
         $form->handleRequest($request);
-        if ($form->isSubmitted()) {
-            $datedebut=$form->get('start_day')->getData() ;
-            $datefin= $form->get('end_day')->getData() ;
-            //calculer la différence entre deux date
-            $date3 = strtotime($datedebut);
-            $date4 = strtotime($datefin);
-            $nbJoursTimestamp = $date4 - $date3;
-            $nbJours = round($nbJoursTimestamp / 86400) + 1;
-            $conge->setStartDay($form->get('start_day')->getData());
-            $conge->setEndDay($form->get('end_day')->getData());
-            $conge->setTypeConge($form->get('type_conge')->getData());
-            $conge->setStatuts("en attente");
-            $conge->setDiscription($form->get('discription')->getData());
-            $conge->setNombredujour($nbJours);
-            $conge->setUserProfile($userprofile);
-            $entityManager->persist($conge);
+        if ($form->isSubmitted())
+        {
+           /* $demanderh->setUserProfile($userprofile);
+              $entityManager->persist($demanderh);
             $entityManager->flush();
-            return $this->redirectToRoute('congelist');
+            return $this->redirectToRoute('receptionlist');
+           */
         }
-        return $this->render('conge/demandeconge.html.twig',['registrationForm' => $form->createView(),]);
+        return $this->render('demanderh/demanderh.html.twig',['registrationForm' => $form->createView(),]);
     }
-*/
+
 
 }
---
+++ src/Entity/Reception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Entity;
+
+class Reception
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    #[ORM\Column(length: 180)]
+    private ?string $discription = null;
+
+}
