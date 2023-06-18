7639d6fd3c015a0fe9c541fa58398f262b65967f
Maher Kefi <kefi.maher@esprit.tn>
2023-06-18 18:02:04 +0100

    18 06 2023  18:01

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,10 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoginController extends  AbstractController
 {
     #[Route('/dashbroad', name: 'dashbroad')]
-    public function dashbroadAction(ManagerRegistry $doctrine ) : Response
+    public function dashbroadAction(ManagerRegistry $doctrine): Response
     {
-        $User = $doctrine->getRepository(User::class);
-        $listuser = $User->findAll();
-        return $this->render('dashbroad/dashbroad.html.twig', array('users' => $listuser));
+            $userrole = $this->getUser()->getRoles();
+            $role = $userrole[0];
+                if ($role == "ROLE_ADMIN")
+                {
+                    $User = $doctrine->getRepository(User::class);
+                    $listuser = $User->findAll();
+                      return $this->render('dashbroad/dashbroad.html.twig', array('users' => $listuser));
+                }
+                else
+                {
+                    return $this->redirectToRoute('demandeconge');
+                }
+            return $this->redirectToRoute('app_logout');
     }
-    }
+}
+
