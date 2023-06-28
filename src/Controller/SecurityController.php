742998aea475d7462af9f1c0dae57c1161abae11
Maher Kefi <kefi.maher@esprit.tn>
2023-06-28 17:50:30 +0100

    28 06 2023  17:50

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,16 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    #[Route(path: '/password', name: 'password')]
+    public function password(): Response
+    {
+        $mail=get('email')->getData() ;
+        echo $mail ;
+        die();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
