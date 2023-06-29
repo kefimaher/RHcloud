261cf11da2eec05963312f87914381eb88c6aa98
Maher Kefi <kefi.maher@esprit.tn>
2023-06-29 17:44:41 +0100

    29 06 2023  17:44

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Controller;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,25 +11,28 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // LOGIN
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-
     #[Route(path: '/password', name: 'password')]
-    public function password(): Response
-    {
-     //   $mail=$form->get('email')->getData() ;
+    public function password(ManagerRegistry $doctrine): Response
+    {   // PASSWORD RECUPURATION
         $mail=$_POST['email'] ;
-        echo $mail ;
-        die();
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+        $user = $doctrine->getRepository(User::class)->findOneBy(array('email' => $mail));
+        if ($user)
+        {
+            $password = $user->getRealpassword() ;
 
+            // send mail and echo message
+        }
+        return $this->redirectToRoute('app_login');
+    }
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
+        // LOG OUT
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
